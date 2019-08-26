223605de42ff526ef54fe93eb84593bd5f51d9f0
shuk <46439764+shukehi@users.noreply.github.com>
2019-08-26 15:00:13 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,32 @@
+<?php
+define('ROOT_PART', Root_part());
+define('APIKEY', 'AIzaSyCXQESH0S84m6WSG0nOqV8RFUHF9889xGQ');
+define('GJ_CODE', 'US');
+define('SITE_NAME', 'YOU2BE');
+define('TITLENAME', 'YOU2BE');
+define('EN2DEKEY', '32123wwerwer323tgfdgds');
+define('EMAIL', 'skeghi@gmail.com');
+define('NAME', 'admin');
+define('PASSWORD', 'admin');
+define('LOGINHTML', '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="Login.css"/>
+</head>
+<body>
+<center>
+    <p id="login">
+    <h1>µÇÂ½</h1>
+    <form method="post" style="margin:0px auto;">
+        <input type="text" required="required" placeholder="ÓÃ»§Ãû" name="name"></input>
+        <br>
+        <input type="password" required="required" placeholder="ÃÜÂë" name="password"></input>
+        <button class="but" type="submit">µÇÂ¼</button>
+    </form>
+    </p>
+</center>
+</body>
+</html>');
+?>
